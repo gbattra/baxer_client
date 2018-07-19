b42780c5f81fc6a3260e5b6de4b32224ec981239
G. Broderick Attra <greg.attra@gmail.com>
2018-07-18 21:27:54 -0400

    fixed css error

--- src/app/utils/components/nav-button.js
+++ src/app/utils/components/nav-button.js
@@ -31,7 +31,7 @@ class NavButton extends React.Component {
     const alignment = this.props.icon ? 'left' : 'center'
     return (
       <Link to={this.props.route.path}>
-        <Button basic className='fw' onClick={this.onButtonClick}>
+        <Button basic fluid onClick={this.onButtonClick}>
           <Header as='h4' textAlign={alignment} style={{'textTransform':'capitalize'}}>
           {this.props.icon ? (
             <Icon circular bordered name='arrow left' size='large' />
